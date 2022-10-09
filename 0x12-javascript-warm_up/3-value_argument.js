e0d5c43015cec20d4db1887803993bc87a97d2b1
SeifeT <101413427+SeifeT@users.noreply.github.com>
2022-10-09 19:15:04 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-// script that prints the first argument passed to it
-const arg = process.argv.slice(2);
-if (!arg[0]) {
-  console.log('No argument');
-} else {
-  console.log(arg[0]);
-}
